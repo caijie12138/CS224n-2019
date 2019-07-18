3cc35a2f5ef6eec697fb40288f471c0c44f32b7e
Sept_C <774932729@qq.com>
2019-07-18 15:05:55 +0800

    add DQN

--- debug.py
+++ debug.py
@@ -17,6 +17,23 @@ def findDuplicates(nums):
                     break
         return res
 
+@pysnooper.snoop()
+def transpose(A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = []
+        for i in range(len(A[0])):
+            res.append([A[0][i]])
+        for col in range(1,len(res)):
+            for i in range(len(A[0])):
+                res[i].append(A[col][i])
+        return res
+
+
 
 if __name__=='__main__':
-    findDuplicates([4,3,2,7,8,2,3,1])
+    # findDuplicates([4,3,2,7,8,2,3,1])
+    x = [[1,2,3],[4,5,6]]
+    transpose(x)
